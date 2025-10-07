7f2140069dc03ede7fd9c6c2b847cba20d63a508
katPanek <91321969+katPanek@users.noreply.github.com>
2025-10-07 10:11:53 +0200

    Implement ProductCreateFormWithModal to integrate media management in product creation

--- src/routes/products/product-create/product-create.tsx
+++ src/routes/products/product-create/product-create.tsx
@@ -1,8 +1,9 @@
 import { useTranslation } from "react-i18next"
-import { RouteFocusModal } from "../../../components/modals"
+import { RouteFocusModal, StackedFocusModal, useStackedModal } from "../../../components/modals"
 import { useSalesChannels } from "../../../hooks/api"
 import { useStore } from "../../../hooks/api/store"
 import { ProductCreateForm } from "./components/product-create-form/product-create-form"
+import { VariantMediaView } from "./components/variant-media-view/variant-media-view"
 
 export const ProductCreate = () => {
   const { t } = useTranslation()
@@ -24,8 +25,52 @@ export const ProductCreate = () => {
         <span className="sr-only">{t("products.create.description")}</span>
       </RouteFocusModal.Description>
       {ready && (
-        <ProductCreateForm defaultChannel={sales_channels[0]} store={store} />
+        <ProductCreateFormWithModal
+          defaultChannel={sales_channels[0]}
+          store={store}
+        />
       )}
     </RouteFocusModal>
   )
 }
+
+// Wrapper component that includes both form and modal
+const ProductCreateFormWithModal = ({
+  defaultChannel,
+  store
+}: {
+  defaultChannel: any
+  store: any
+}) => {
+  const { setIsOpen } = useStackedModal()
+
+  return (
+    <>
+      <ProductCreateForm
+        defaultChannel={defaultChannel}
+        store={store}
+        onOpenMediaModal={() => setIsOpen("variant-media-modal", true)}
+      />
+
+      {/* Always render modal, but control visibility */}
+      <StackedFocusModal
+        id="variant-media-modal"
+        onOpenChangeCallback={(open) => {
+          if (!open) {
+            setIsOpen("variant-media-modal", false)
+          }
+        }}
+      >
+        <VariantMediaView
+          variantId="new-variant"
+          variantTitle="Sample Variant"
+          onClose={() => setIsOpen("variant-media-modal", false)}
+          onSubmit={() => setIsOpen("variant-media-modal", false)}
+        />
+      </StackedFocusModal>
+    </>
+  )
+}
+
+
+

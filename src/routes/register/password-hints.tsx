a16defe751acca0538e99f1e520fd25612678f3e
Kamil Kiliasiński <kamilkiliasinski@gmail.com>
2025-12-18 11:25:46 +0100

    Enhanced password validation by adding localization support for password rules and refactoring `PasswordValidator` for reusability.

--- src/routes/register/password-hints.tsx
+++ src/routes/register/password-hints.tsx
@@ -1,10 +1,13 @@
 'use client';
 
-import { useEffect, useState } from 'react';
+import { type FC, useEffect, useState } from 'react';
+
+
 
 import { CheckCircle } from '@medusajs/icons';
 import { Container } from '@medusajs/ui';
 import { clsx } from 'clsx';
+import { useTranslation } from 'react-i18next';
 
 function validatePassword(password: string) {
   const errors = {
@@ -20,6 +23,38 @@ function validatePassword(password: string) {
   };
 }
 
+const rules = {
+  isValid: false,
+  lower: false,
+  upper: false,
+  '8chars': false,
+  symbolOrDigit: false
+}
+
+const PasswordRule: FC<{ isValid: boolean; ruleName: keyof typeof rules }> = ({ ruleName, isValid }) => {
+
+  const { t } = useTranslation();
+  if (ruleName === "isValid") return
+
+  const rulesText: Record<Exclude<keyof typeof rules, "isValid">,string> = {
+    lower: t('validation.rules.lower'),
+    upper: t('validation.rules.upper'),
+    '8chars': t('validation.rules.8chars'),
+    symbolOrDigit: t('validation.rules.symbolOrDigit')
+  }
+
+  return (
+    <p
+      className={clsx(
+        'flex items-center gap-2 text-xs',
+        isValid ? 'text-red-700' : 'text-green-700'
+      )}
+    >
+      <CheckCircle /> {rulesText[ruleName]}
+    </p>
+  );
+};
+
 export const PasswordValidator = ({
   password,
   setError
@@ -27,13 +62,7 @@ export const PasswordValidator = ({
   password: string;
   setError: (error: any) => void;
 }) => {
-  const [newPasswordError, setNewPasswordError] = useState({
-    isValid: false,
-    lower: false,
-    upper: false,
-    '8chars': false,
-    symbolOrDigit: false
-  });
+  const [newPasswordError, setNewPasswordError] = useState(rules)
 
   useEffect(() => {
     const validation = validatePassword(password);
@@ -57,38 +86,11 @@ export const PasswordValidator = ({
 
   return (
     <Container className="p-2 flex flex-col gap-y-1">
-      <p
-        className={clsx(
-          'text-xs flex items-center gap-2',
-          newPasswordError['8chars'] ? 'text-red-700' : 'text-green-700'
-        )}
-      >
-        <CheckCircle /> At least 8 characters
-      </p>
-      <p
-        className={clsx(
-          'text-xs flex items-center gap-2',
-          newPasswordError['lower'] ? 'text-red-700' : 'text-green-700'
-        )}
-      >
-        <CheckCircle /> One lowercase letter
-      </p>
-      <p
-        className={clsx(
-          'text-xs flex items-center gap-2',
-          newPasswordError['upper'] ? 'text-red-700' : 'text-green-700'
-        )}
-      >
-        <CheckCircle /> One uppercase letter
-      </p>
-      <p
-        className={clsx(
-          'text-xs flex items-center gap-2',
-          newPasswordError['symbolOrDigit'] ? 'text-red-700' : 'text-green-700'
-        )}
-      >
-        <CheckCircle /> One number or symbol
-      </p>
+      {
+        (Object.keys(newPasswordError) as (keyof typeof rules)[]).map(k => (
+          <PasswordRule key={k} ruleName={k} isValid={newPasswordError[k]} />
+        ))
+      }
     </Container>
   );
 };

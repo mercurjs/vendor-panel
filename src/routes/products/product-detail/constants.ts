c0e307f842ed27a36f38ab3f0aea6c8d994651d4
kacpergumieniuk <kacpergumieniuk@gmail.com>
2026-02-02 09:46:01 +0100

    Fix product info

--- src/routes/products/product-detail/constants.ts
+++ src/routes/products/product-detail/constants.ts
@@ -1,6 +1,13 @@
 import { getLinkedFields } from '../../../extensions';
 
 const BASE_PRODUCT_DETAIL_FIELDS = [
+  // Base fields used directly in the UI
+  "id",
+  "title",
+  "status",
+  "handle",
+  "description",
+  "discountable",
   // Media
   "*images",
   "thumbnail",
